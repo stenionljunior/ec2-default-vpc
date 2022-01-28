8baaea96f398645ab2d6ca01cb9292317fae475d
Stenio Junior <56360594+stenionljunior@users.noreply.github.com>
2022-01-28 13:17:32 -0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,51 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.50.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
+
+
+resource "aws_security_group" "aws_sg" {
+  name = "security group from terraform"
+
+  ingress {
+    description = "SSH from the internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_instance" "aws_ins_web" {
+
+  ami                         = "ami-04505e74c0741db8d"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.aws_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "ec2key-lab"
+
+  tags = {
+    Name = "my instance"
+  }
+
+}
+
+output "instance_ip" {
+  value = aws_instance.aws_ins_web.public_ip
+}
